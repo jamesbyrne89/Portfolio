28675665ec1898d9503590c7ab2052ec58be273d
jamesbyrne89 <mail@jamestbyrne.com>
2016-09-24 14:47:24 +0100

    Responsive menu design

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,37 @@
 $(document).ready();
 
+
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+      console.log('scrolled');
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile <a href="http://www.jqueryscript.net/tags.php?/Navigation/">Navigation</a>
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav')
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h ul a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+
+
+
+
 $("img.lazy").lazyload();
 
 
