b9ca460d0a48b72506a5ee5a9b39d34ae48d3ea1
jamesbyrne89 <mail@jamestbyrne.com>
2018-05-31 23:19:27 +0100

    Remove unecessary external libraries and add lazy loading to images

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,109 +1,79 @@
 /**
  * Sticky header that slides into view only when the user scrolls up
  */
-
-(function stickyHeader() {
+document.addEventListener('DOMContentLoaded', function() {
+  (function stickyHeader() {
     let previous = window.scrollY;
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 180 && window.scrollY > previous) {
-            $('.site-header').removeClass('sticky');
-            previous = window.scrollY;
-        } else if (window.scrollY > 180 && window.scrollY < previous) {
-            $('.site-header').addClass('sticky');
-            previous = window.scrollY;
-        } else if (window.scrollY < 180) {
-            $('.site-header').removeClass('sticky');
-            previous = window.scrollY;
-        } else {
-            return;
-        }
+      if (window.scrollY > 180 && window.scrollY > previous) {
+        $('.site-header').removeClass('sticky');
+        previous = window.scrollY;
+      } else if (window.scrollY > 180 && window.scrollY < previous) {
+        $('.site-header').addClass('sticky');
+        previous = window.scrollY;
+      } else if (window.scrollY < 80) {
+        $('.site-header').removeClass('sticky');
+        previous = window.scrollY;
+      } else {
+        return;
+      }
     });
-})();
+  })();
 
-
-// MOBILE NAVIGATION
-$(function($) {
+  // MOBILE NAVIGATION
+  $(function($) {
     $('.menu-btn').on('click', function() {
-        $('.site-nav').toggleClass('menu--active');
-        $('.site-nav__item').toggleClass('site-nav__item--active');
-        $('.menu-btn span:first-child').toggleClass('opened');
-        $('.menu-btn span:nth-child(2)').toggleClass('hidden');
-        $('.menu-btn span:last-child').toggleClass('opened');
+      $('.site-nav').toggleClass('menu--active');
+      $('.site-nav__item').toggleClass('site-nav__item--active');
+      $('.menu-btn span:first-child').toggleClass('opened');
+      $('.menu-btn span:nth-child(2)').toggleClass('hidden');
+      $('.menu-btn span:last-child').toggleClass('opened');
     });
-});
+  });
 
-$(function($) {
+  $(function($) {
     $('.menu-link').on('click', function() {
-        $('.responsive-menu').addClass('expand')
-        $('.menu-btn span:first-child').removeClass('opened')
-        $('.menu-btn span:nth-child(2)').removeClass('hidden')
-        $('.menu-btn span:last-child').removeClass('opened')
+      $('.responsive-menu').addClass('expand');
+      $('.menu-btn span:first-child').removeClass('opened');
+      $('.menu-btn span:nth-child(2)').removeClass('hidden');
+      $('.menu-btn span:last-child').removeClass('opened');
     });
-});
+  });
 
-$(function($) {
+  $(function($) {
     $('.menu-link').click(function() {
-        $('.responsive-menu').toggleClass('expand')
+      $('.responsive-menu').toggleClass('expand');
     });
-});
+  });
 
-$("img.lazy").lazyload({
-    threshold: -900
-});
+  var $aboutSection = $('#about');
+  var $mouse = $('.mouse');
 
+  $mouse.addClass('fade-in');
 
-
-// SLIDE IN ANIMATIONS
-
-// This function needs fixing!!!
-
-function animateIn(el, offset) {
-    var $element = $(el);
-
-    $element.waypoint(function(direction) {
-        if (direction == 'down') {
-            $element.addClass('sideintro', 'delayed-text-5')
-            $element.removeClass('animated fadeOut')
-        } else {
-            $element.removeClass('sideintro', 'delayed-text-5')
-            $element.addClass('animated fadeOut')
-        }
-    }, {
-        offset: offset
-    });
-}
-
-
-
-
-var $aboutSection = $('#about');
-var $mouse = $('.mouse');
-
-$mouse.addClass('fade-in');
-
-$aboutSection.waypoint(function(direction) {
-    if (direction == 'down') {
+  $aboutSection.waypoint(
+    function(direction) {
+      if (direction == 'down') {
         $mouse.addClass('fade-out');
         $mouse.removeClass('fade-in');
-    } else {
+      } else {
         $mouse.removeClass('fade-in');
         $mouse.removeClass('fade-out');
-    }
-}, {
-    offset: '60%'
-});
+      }
+    },
+    { offset: '60%' },
+  );
 
+  // ABOUT ME SECTION //
+  var $about = $('.about-section p');
+  var $aboutTitle = $('.about-section-header');
+  var $skillsTitle = $('.about-text__subtitle');
+  var $skillsList = $('.skills-list ul');
+  var $skillIcon = $('.tech-icon-wrapper');
 
-// ABOUT ME SECTION //
-
-var $about = $('.about-section p');
-var $aboutTitle = $('.about-section-header');
-var $skillsTitle = $('.about-text__subtitle');
-var $skillsList = $('.skills-list ul');
-var $skillIcon = $('.tech-icon-wrapper');
-
-$about.waypoint(function(direction) {
-    if (direction == 'down') {
+  $about.waypoint(
+    function(direction) {
+      if (direction == 'down') {
         $about.addClass('fade-in');
         $about.removeClass('fade-out');
         $aboutTitle.addClass('slide-left');
@@ -111,57 +81,39 @@ $about.waypoint(function(direction) {
         $skillsTitle.addClass('slide-left');
         $skillsTitle.removeClass('fade-out');
         $skillIcon.addClass('slide-up');
-    } else {
-        // $about.removeClass('fade-in');
-        // $about.addClass('fade-out');
-        // $aboutTitle.addClass('fade-out');
-        // $aboutTitle.removeClass('slide-left');
-        // $skillsTitle.addClass('fade-out');
-        // $skillsTitle.removeClass('slide-left');
-        // $skillIcon.removeClass('slide-up');
+      } else {}
+    },
+    { offset: '70%' },
+  );
 
-    }
-}, {
-    offset: '70%'
-});
+  // PROJECTS SECTION //
+  var $projectsTitle = $('.projects-section-header');
+  var $projects = $('.projects-gallery-intro');
+  var $gallery = $('.gallery');
 
-
-// PROJECTS SECTION //
-
-var $projectsTitle = $('.projects-section-header');
-var $projects = $('.projects-gallery-intro');
-var $gallery = $('.gallery');
-
-$projects.waypoint(function(direction) {
-    if (direction == 'down') {
+  $projects.waypoint(
+    function(direction) {
+      if (direction == 'down') {
         $projects.addClass('fade-in');
         $projects.removeClass('fade-out');
         $projectsTitle.addClass('slide-left');
         $projectsTitle.removeClass('fade-out');
         $gallery.addClass('fade-in');
         $gallery.removeClass('fade-out');
-    } else {
-        // $projects.removeClass('fade-in');
-        // $projects.addClass('fade-out');
-        // $projectsTitle.addClass('fade-out');
-        // $projectsTitle.removeClass('slide-left');
-        // $gallery.removeClass('fade-in');
-        // $gallery.addClass('fade-out');
-    }
-}, {
-    offset: '70%'
-});
+      } else {}
+    },
+    { offset: '70%' },
+  );
 
+  // CONTACT SECTION //
+  var $contact = $('.contact-section p');
+  var $contactTitle = $('.contact-section-header');
+  var $emailBtn = $('.btn-contact');
+  var $socialIcons = $('.social-icon');
 
-// CONTACT SECTION //
-
-var $contact = $('.contact-section p');
-var $contactTitle = $('.contact-section-header');
-var $emailBtn = $('.btn-contact');
-var $socialIcons = $('.social-icon');
-
-$contact.waypoint(function(direction) {
-    if (direction == 'down') {
+  $contact.waypoint(
+    function(direction) {
+      if (direction == 'down') {
         $contact.addClass('fade-in');
         $contact.removeClass('fade-out');
         $contactTitle.addClass('slide-left');
@@ -170,75 +122,56 @@ $contact.waypoint(function(direction) {
         $emailBtn.removeClass('fade-out');
         $socialIcons.addClass('fade-in');
         $socialIcons.removeClass('fade-out');
-    } else {
-        // $contact.removeClass('fade-in');
-        // $contact.addClass('fade-out');
-        // $contactTitle.addClass('fade-out');
-        // $contactTitle.removeClass('slide-left');
-        // $emailBtn.removeClass('fade-in');
-        // $emailBtn.addClass('fade-out');
-        // $socialIcons.removeClass('fade-in');
-        // $socialIcons.addClass('fade-out');
-    }
-}, {
-    offset: '80%'
-});
+      } else {}
+    },
+    { offset: '80%' },
+  );
 
+  // PROJECTS PAGE - SUMMARY SECTION //
+  var $projectSummaryRight = $('.summary-right');
+  var $projectSummaryLeft = $('.summary-left');
+  var $fullSummary = $('.details__full-summary p');
 
-// PROJECTS PAGE - SUMMARY SECTION //
-
-var $projectSummaryRight = $('.summary-right');
-var $projectSummaryLeft = $('.summary-left');
-var $fullSummary = $('.details__full-summary p');
-
-$projectSummaryRight.waypoint(function(direction) {
-    if (direction == 'down') {
+  $projectSummaryRight.waypoint(
+    function(direction) {
+      if (direction == 'down') {
         $projectSummaryLeft.addClass('slide-left');
         $projectSummaryRight.addClass('fade-in');
-    } else {
+      } else {}
+    },
+    { offset: '80%' },
+  );
 
-    }
-}, {
-    offset: '80%'
-});
-
-$fullSummary.waypoint(function(direction) {
-    if (direction == 'down') {
+  $fullSummary.waypoint(
+    function(direction) {
+      if (direction == 'down') {
         $fullSummary.addClass('fade-in');
-    } else {
+      } else {}
+    },
+    { offset: '80%' },
+  );
 
-    }
-}, {
-    offset: '80%'
-});
+  // PROJECTS PAGE - TECH SECTION //
+  var $techTitle = $('.tech-section-header');
+  var $details = $('.details-wrapper');
+  var $techIcon = $('.tech-icon-wrapper');
 
-// PROJECTS PAGE - TECH SECTION //
-
-var $techTitle = $('.tech-section-header');
-var $details = $('.details-wrapper');
-var $techIcon = $('.tech-icon-wrapper');
-
-$details.waypoint(function(direction) {
-    if (direction == 'down') {
-        // $contact.addClass('fade-in');
+  $details.waypoint(
+    function(direction) {
+      if (direction == 'down') {
         $techTitle.addClass('slide-left');
         $techTitle.removeClass('fade-out');
         $techIcon.addClass('slide-up');
-    } else {
-        // $contact.removeClass('fade-in');
-        // $contact.addClass('fade-out');
-        // $contactTitle.addClass('fade-out');
-        // $contactTitle.removeClass('slide-left');
-        // $emailBtn.removeClass('fade-in');
-        // $emailBtn.addClass('fade-out');
-        // $socialIcons.removeClass('fade-in');
-        // $socialIcons.addClass('fade-out');
-    }
-}, {
-    offset: '80%'
+      } else {}
+    },
+    { offset: '80%' },
+  );
+
+  var scroll = new SmoothScroll('a.menu-link');
+
+  let year = new Date().getFullYear() || null;
+
+  document.querySelector(
+    '.site-footer',
+  ).innerText = `Site design by James Byrne. © ${year}`;
 });
-
-let date = new Date();
-let year = date.getFullYear();
-
-document.querySelectorAll('.site-footer').innerText = `Site design by James Byrne. © ${year}`;
