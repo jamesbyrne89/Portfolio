1da9a88220b49b23bc6dd4058eda0e82f49f201d
jamesbyrne89 <mail@jamestbyrne.com>
2019-08-01 23:33:56 +0100

    Clone starter repo and add first component

---
+++ src/components/layout.js
@@ -0,0 +1,39 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import HeroSection from "./HeroSection"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <HeroSection text="Hello. I'm James Byrne, a frontend developer based in London." />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
